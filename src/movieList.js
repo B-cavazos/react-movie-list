051327dcffd17035bb4a0d353fb0b00ad2d1f357
Ludwin Granados <ludwingranados@gmail.com>
2021-03-12 12:01:11 -0800

    updated App and movieList js files

--- src/movieList.js
+++ src/movieList.js
@@ -1,23 +1,98 @@
 import React from 'react';
 
 
-const TableBody = () => {
-  return <tbody />
-}
 
-const TableComponent = () => {
-  render() {
-    const {movieData} = this.props
+const TableComponent = ({ movies }) => {
+  const showDetails = movieId => {
+    // debugger;
 
-    return (
-      <table>
+    let foundMovie = movies.find(movie => movie.id === movieId);
 
-        <TableBody movieData={movieData} />
+    if (!foundMovie) {
+      alert(`Sorry! Could not find the movie you where looking for!`);
+    } else {
+      alert(`${foundMovie.title} is a ${foundMovie.genre} flick that came out in ${foundMovie.year}!!`);
+    }
+  };
 
-      </table>
-    )
-  }
+  return (
+    <div className='row'>
+      <div className='col-6 offset-3'>
+        <ul className='list-group'>
+          {movies.map((movie, index) => {
+            return (
+              <li className='list-group-item' key={index}>
+
+                <a onClick=''><big> {movie.title} </big></a>
+                <br></br>
+
+                 {movie.genre} | {movie.year}
+                <br></br>
+
+                <a
+                  href='javascript:void(0)'
+                  onClick={() => showDetails(movie.id)}>
+                    <button className='btn-primary'>Click for details</button>
+                </a>
+
+                <br></br>
+
+              </li>
+
+
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
+
+const images = [
+  { id: 1, src: 'https://m.media-amazon.com/images/M/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM@._V1_UX182_CR0,0,182,268_AL_.jpg', title: 'foo', description: 'bar' },
+  { id: 2, src: './assets/image02.jpg', title: 'foo', description: 'bar' },
+  { id: 3, src: './assets/image03.jpg', title: 'foo', description: 'bar' },
+  { id: 4, src: './assets/image04.jpg', title: 'foo', description: 'bar' },
+  { id: 5, src: './assets/image05.jpg', title: 'foo', description: 'bar' },
+
+];
+
+
+
+{ images.map(({id, src, title, description}) => <img key={id} src={src} title={title} alt={description} />)}
+
+
+// const images = [
+//   { id: 1, src: './assets/image01.jpg', title: 'foo', description: 'bar' },
+//   { id: 2, src: './assets/image02.jpg', title: 'foo', description: 'bar' },
+//   { id: 3, src: './assets/image03.jpg', title: 'foo', description: 'bar' },
+//   { id: 4, src: './assets/image04.jpg', title: 'foo', description: 'bar' },
+//   { id: 5, src: './assets/image05.jpg', title: 'foo', description: 'bar' },
+//   ...etc
+// ];
+
+// function imageLoader() {
+//   return images;
+// }
+
+
+// const TableBody = () => {
+//   return <tbody />
+// }
+
+// const TableComponent = () => {
+//   render() {
+//     const {movieData} = this.props
+
+//     return (
+//       <table>
+
+//         <TableBody movieData={movieData} />
+
+//       </table>
+//     )
+//   }
+// };
 
 
 
@@ -41,7 +116,7 @@ const TableComponent = () => {
 // };
 
 
-// const MovieListComponent = movies => {
+// const TableComponent = ({movies}) => {
 //   return (
 //       <ul>
 //         {movies.map((movie, index) => (
@@ -51,7 +126,7 @@ const TableComponent = () => {
 //             <p>{movie.year}</p>
 //           </li>
 //         ))}
-//       </ul>;
+//       </ul>
 //     );
 //   }
 
